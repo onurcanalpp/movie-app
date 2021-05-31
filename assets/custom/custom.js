e4b4dde824e96bc14cc989314165a1cd55116200
onurcanalpp <onurcanalpp@gmail.com>
2021-05-31 09:27:36 +0300

    all movies getting with api  key and showing homepage

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,5 +1,6 @@
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const movieList = document.querySelector(".movie-list");
 
 async function getMovies() {
     const resp = await fetch(APIURL);
@@ -7,10 +8,19 @@ async function getMovies() {
       
     console.log(respData);
 
-    respData.results.forEach(movie => {
-        const img = document.createElement('img');
-        img.src = IMGPATH + movie.poster_path;
-        document.body.appendChild(img);
+    respData.results.forEach(movie =>{
+        const { poster_path, title, vote_average } = movie;
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span>${vote_average}</span>
+            </div>
+        `;
+
+        movieList.appendChild(movieElement);
     });
 
     return respData;
